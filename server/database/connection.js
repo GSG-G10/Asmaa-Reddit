7220f406d28a26feeac69b5a345e9ca7fc18a326
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-09-01 23:38:04 +0300

    add unsaved changes #14

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,3 +1,32 @@
 require('env2')('.env');
 
 const { Pool } = require('pg');
+
+const {
+  DATABASE_URL, DB_URL, DB_TEST_URL, NODE_ENV,
+} = process.env;
+
+let dburl = '';
+
+switch (NODE_ENV) {
+  case 'production':
+    dburl = DATABASE_URL;
+    break;
+  case 'development':
+    dburl = DB_URL;
+    break;
+  case 'test':
+    dburl = DB_TEST_URL;
+    break;
+  default:
+    throw new Error('No Database is founded !');
+}
+
+const options = {
+  connectionString: dburl,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+module.exports = new Pool(options);
