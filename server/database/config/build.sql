f4b5a00c1434536aaf038fc4c0076da742070b64
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-09-02 10:53:28 +0300

    write build sql and create insert sql and insert default values #16

---
+++ server/database/config/build.sql
@@ -0,0 +1,34 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, posts, comments CASCADE;
+
+CREATE TABLE IF NOT EXISTS users(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(100) UNIQUE NOT NULL,
+  email VARCHAR(200) UNIQUE NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  img_url TEXT
+);
+
+CREATE TABLE IF NOT EXISTS posts(
+  id SERIAL PRIMARY KEY,
+  post_text TEXT NOT NULL,
+  post_img TEXT,
+  likes INT DEFAULT 0,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS comments(
+  id SERIAL PRIMARY KEY,
+  comment_text TEXT NOT NULL,
+  likes INT DEFAULT 0,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  post_id INT,
+  FOREIGN KEY (post_id) REFERENCES posts(id)
+);
+
+COMMIT;
